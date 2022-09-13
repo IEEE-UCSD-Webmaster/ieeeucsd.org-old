6b830a7a3a14c9c0b772f05ba3d897e27a83c55d
JilianaTiu <jilianatiu@gmail.com>
2022-09-13 16:04:39 -0700

    update projects content

--- src/public/projects.tsx
+++ src/public/projects.tsx
@@ -20,25 +20,19 @@ class Main extends React.Component<MainProps, MainState> {
 	public render() {
 		return <>
 			<Splash cta="Projects" delay={2000} backgrounds={["img/backgrounds/robocar.png"]}></Splash>
-			<DefaultSection title="Projects" paragraphs={[
-				"IEEE hosts Robocup Soccer, an annual project where teams develop six robots to compete with other teams during Robofest. Join this hands-on project to explore computer vision, mechanical design, and microcontroller development!"
-				,"Gain project experience through IEEE's Quarterly Projects where teams fully develop a project from scratch within 10 weeks!"
+			<DefaultSection title="RoboCup" paragraphs={[
+				"RoboCup is an international scientific initiative with the goal to advance the state of the art of intelligent robots. When established in 1997, the original mission was to field a team of robots capable of winning against the human soccer World Cup champions by 2050."
+				,"IEEE hosts Robocup Soccer, an annual project where teams develop six robots to compete with other teams during Robofest. Join this hands-on project to explore computer vision, mechanical design, and microcontroller development!"
 			]}></DefaultSection>
-			<div className="project-space">
-				<div className="ps-title">Robocup</div>
-				<p>“<a href="https://www.robocup.org/">RoboCup</a> is an international scientific initiative with the goal to advance the state of the art of intelligent robots. When established in 1997, the original mission was to field a team of robots capable of winning against the human soccer World Cup champions by 2050.”</p>
-				<p>IEEE hosts Robocup Soccer, an annual project where teams develop six robots to compete with other teams during Robofest. Join this hands-on project to explore computer vision, mechanical design, and microcontroller development!</p>
-			</div>
-			<div className="project-space">
-				<div className="ps-title">Quarterly Projects</div>
-				<p>Gain project experience through IEEE's Quarterly Projects where teams fully develop a project from scratch within 10 weeks!</p>
-				<a href="https://forms.gle/eW6e1i3vWCdBj7Vn6">Apply here!</a>
-			</div>
+			<DefaultSection title="Quarterly Projects" paragraphs={[
+				"Gain project experience through IEEE's Quarterly Projects where teams fully develop a project from scratch within 10 weeks!",
+				"<a className='visit-us' href='https://forms.gle/eW6e1i3vWCdBj7Vn6'>Apply here!</a>"
+			]}></DefaultSection>
 			<DefaultSection className={"past-proj"} title="Past Projects">
 				<div className="cards">
-					<InvolveBox boxTitle="" image="img/backgrounds/fa21social.png" description="Meet fellow IEEE members!"></InvolveBox>
-					<InvolveBox boxTitle="" image="img/backgrounds/robofest.png" description="Learn new skills!"></InvolveBox>
-					<InvolveBox boxTitle="" image="img/backgrounds/gbm.png" description="Build our amazing community!"></InvolveBox>
+					<InvolveBox boxTitle="" image="img/backgrounds/robocar.png" description="'22 Robocar Competition"></InvolveBox>
+					<InvolveBox boxTitle="" image="img/backgrounds/micromouse.png" description="'22 Micromouse Competition"></InvolveBox>
+					<InvolveBox boxTitle="" image="img/backgrounds/robocarteam.png" description="IEEE Team Meeting"></InvolveBox>
 				</div>
 			</DefaultSection>
 			<DefaultSection title="Have questions? Contact us!">
