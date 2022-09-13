52cfcf86a6948524d8a3bf78ca9e3e7379ce8370
JilianaTiu <jilianatiu@gmail.com>
2022-09-13 16:15:14 -0700

    update projects styles

--- src/public/projects.tsx
+++ src/public/projects.tsx
@@ -21,18 +21,21 @@ class Main extends React.Component<MainProps, MainState> {
 		return <>
 			<Splash cta="Projects" delay={2000} backgrounds={["img/backgrounds/robocar.png"]}></Splash>
 			<DefaultSection title="RoboCup" paragraphs={[
-				"RoboCup is an international scientific initiative with the goal to advance the state of the art of intelligent robots. When established in 1997, the original mission was to field a team of robots capable of winning against the human soccer World Cup champions by 2050."
+				"\"RoboCup is an international scientific initiative with the goal to advance the state of the art of intelligent robots. When established in 1997, the original mission was to field a team of robots capable of winning against the human soccer World Cup champions by 2050.\""
 				,"IEEE hosts Robocup Soccer, an annual project where teams develop six robots to compete with other teams during Robofest. Join this hands-on project to explore computer vision, mechanical design, and microcontroller development!"
-			]}></DefaultSection>
+			]}>
+				<a className='ex-link' href='https://www.robocup.org/'>More information at RoboCup website</a>
+			</DefaultSection>
 			<DefaultSection title="Quarterly Projects" paragraphs={[
-				"Gain project experience through IEEE's Quarterly Projects where teams fully develop a project from scratch within 10 weeks!",
-				"<a className='visit-us' href='https://forms.gle/eW6e1i3vWCdBj7Vn6'>Apply here!</a>"
-			]}></DefaultSection>
+				"Gain project experience through IEEE's Quarterly Projects where teams fully develop a project from scratch within 10 weeks!"
+			]}>
+				<a className='ex-link' href='https://forms.gle/eW6e1i3vWCdBj7Vn6'>Apply here!</a>
+			</DefaultSection>
 			<DefaultSection className={"past-proj"} title="Past Projects">
 				<div className="cards">
 					<InvolveBox boxTitle="" image="img/backgrounds/robocar.png" description="'22 Robocar Competition"></InvolveBox>
 					<InvolveBox boxTitle="" image="img/backgrounds/micromouse.png" description="'22 Micromouse Competition"></InvolveBox>
-					<InvolveBox boxTitle="" image="img/backgrounds/robocarteam.png" description="IEEE Team Meeting"></InvolveBox>
+					<InvolveBox boxTitle="" image="img/backgrounds/robocarteam.png" description="'22 IEEE Team Meeting"></InvolveBox>
 				</div>
 			</DefaultSection>
 			<DefaultSection title="Have questions? Contact us!">
