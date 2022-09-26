5a3fdae5b27545a8abafe5f4cf0dce42b9e21329
JilianaTiu <jilianatiu@gmail.com>
2022-09-26 11:56:28 -0700

    update events page

--- src/public/events.tsx
+++ src/public/events.tsx
@@ -20,10 +20,18 @@ class Main extends React.Component<MainProps, MainState> {
 	public render() {
 		return <>
 			<TopBar links={ACTIVE_PAGES}></TopBar>
-			<Splash cta="Events" delay={2000} backgrounds={["img/backgrounds/fa21social.png"]}></Splash>
+			<Splash cta="Come out to our events!" delay={2000} backgrounds={["img/backgrounds/fa21social.png"]}></Splash>
 			<DefaultSection title="" paragraphs={[
-				"No events yet... Keep an eye out for new upates!"
+				"Outside the classroom, we believe our events should bring us together and help us refine our skills, foster long lasting connections, give back to the community, and prepare us for our careers.",
+				"In similar fashion, our events are split up each to serve one of these goals: technical, social, K-12 outreach, and professional."
 			]}></DefaultSection>
+			
+			<DefaultSection title="Officer Open Hours">
+				<div>
+					<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles" width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+				</div>
+			</DefaultSection>
+			
 			<DefaultSection title="Have questions? Contact us!">
 				<div className="join-scls">{
 					[...EMAIL, ...SOCIALS].map(n => (
@@ -31,20 +39,6 @@ class Main extends React.Component<MainProps, MainState> {
 					))
 				}</div>
 			</DefaultSection>
-
-			<div id="calendar">
-			<html>
-				<iframe id="cal" src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles"></iframe>
-			</html>
-			</div>
-			
-
-			<DefaultSection title="Officer Open Hours">
-				<div>
-					<iframe src="https://calendar.google.com/calendar/embed?height=600&wkst=1&bgcolor=%23ffffff&ctz=America%2FLos_Angeles" width="800" height="600" frameBorder="0" scrolling="no"></iframe>
-				</div>
-			</DefaultSection>
-			
 
 			<Footer></Footer>
 		</>;
