01c7c0b9d0547050366e5e8e1ef06e492ed301ae
chark1es <charles15851@gmail.com>
2023-10-23 23:28:54 -0700

    updated staff list to current list
    missing avatars from several people

--- src/public/components/CarouselItem.tsx
+++ src/public/components/CarouselItem.tsx
@@ -12,12 +12,20 @@ export default class CarouselItem extends Component<CarouselItemProps> {
     constructor(props: CarouselItemProps) {
         super(props);
         this.state = {};
+
+        this.handleImageError = this.handleImageError.bind(this);
+    }
+    handleImageError(e) {
+        e.target.src = "/assets/img/officers/avatar.jpg"; // Default Image
     }
 
     public render() {
         return (
             <div className="carousel-item">
-                <img src={this.props.photo}></img>
+                <img
+                    src={this.props.photo}
+                    onError={this.handleImageError}
+                ></img>
                 <div className="carousel-name">{this.props.name}</div>
                 <div className="carousel-pos">{this.props.position}</div>
                 <div className="carousel-email">
